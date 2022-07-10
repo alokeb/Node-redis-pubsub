84edaf4edb4c2bc232913c2ddbea34dbef33d016
Aloke <alokeb@gmail.com>
2022-07-10 14:33:46 -0700

    moving to Node-scripts repo

--- start.sh
+++
@@ -1,5 +0,0 @@
-#This is only needed if you're using helm to deploy into a Kubernetes cluster
-
-#Fruit server
-helm delete fruit-server
-helm upgrade -i fruit-server bitnami/node --values=examples/Fruit-server/helm-values.yaml
